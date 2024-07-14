783b59c4ca8fb18c09ce666bc450a477907127e3
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2024-07-14 10:17:31 -0400

    Ref fix

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -16,7 +16,7 @@ import { InputFile } from "node-appwrite/file";
 export const createUser = async (user: CreateUserParams) => {
   try {
     // userId, email (optional), phone (optional), password (optional), name (optional)
-    console.log("HELLO", user);
+    console.log("HELLO User", user);
     const newUser = await users.create(
       ID.unique(),
       user?.email,
